7fe526c332772166cd689adf4ef769c6a00bb6a3
Vindhya-14 <vindhya3321@gmail.com>
2025-05-12 06:39:25 +0530

    commit

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,40 @@
+// const mysql = require("mysql2");
+// const dotenv = require("dotenv");
+// dotenv.config();
+
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
+
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log("MySQL Connected");
+// });
+
+// module.exports = connection;
+
+// db/connection.js
+
 const mysql = require("mysql2");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
-  if (err) throw err;
-  console.log("MySQL Connected");
+  if (err) {
+    console.error("❌ DB connection failed:", err.stack);
+    return;
+  }
+  console.log("✅ Connected to MySQL on Railway");
 });
 
 module.exports = connection;
