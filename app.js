89a3f4c105d6a2848bd442aabe3bb84266b6529e
Vindhya-14 <vindhya3321@gmail.com>
2025-05-12 08:29:30 +0530

    commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const schoolRoutes = require("./routes/schoolRoutes");
 app.use(express.json());
 app.use("/", schoolRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
